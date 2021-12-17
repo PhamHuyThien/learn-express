e69e3b463de4053b9ebf22f421f7087226d6c716
PhamHuyThien <sync.mail.pc@gmail.com>
2021-12-18 01:18:46 +0700

    add music play

--- routers/home.js
+++ routers/home.js
@@ -2,5 +2,6 @@ const router = require("express").Router();
 
 router.get("/", (req, res) => res.render("index"));
 router.get("/login", (req, res) => res.render("login"));
+router.get("/register", (req, res) => res.render("register"));
 
 module.exports = router;
