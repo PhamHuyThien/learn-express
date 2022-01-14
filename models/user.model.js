dd104ed25dff3f3a4d686bc009e0abd5f074c56a
PhamHuyThien <sync.mail.pc@gmail.com>
2022-01-14 20:43:47 +0700

    update user model

--- models/user.model.js
+++ models/user.model.js
@@ -5,12 +5,6 @@ module.exports = mongoose.model("User", mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     username: {
         type: String,
-        validate: {
-            validator: function (text) {
-                return regex.REGEX_USERNAME.test(text.toLowerCase());
-            },
-            message: "tài khoản chỉ bao gồm chữ, số, _ và trên 3 kí tự.",
-        },
         unique: true,
         required: true,
     },
